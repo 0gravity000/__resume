6fdda5c864062a624731b6e6d403db2185032a5f
0gravity <0gravity000@gmail.com>
2021-08-17 15:01:30 +0900

    Improve Welcome page design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,95 +6,67 @@
 
         <title>Resume</title>
 
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/resume.css') }}" rel="stylesheet">
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <!-- Bulma CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+        <!-- navibar -->
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col">
+                    <ul class="nav justify-content-end">
+                    @if (Route::has('login'))
+                        @auth
+                        <li class="nav-item px-2 py-2">
+                            <a class="header_boxlink" href="{{ url('/home') }}">Home</a>
+                        </li>
+                        @else
+                        <li class="nav-item px-2 py-2">
+                            <a class="header_boxlink" href="{{ route('login') }}">Login</a>
+                        </li>
+                            @if (Route::has('register'))
+                            <li class="nav-item px-2 py-2">
+                                <a class="header_boxlink" href="{{ route('register') }}">Register</a>
+                            </li>
+                            @endif
+                        @endauth
+                    @endif
+                    </ul>
+                </div>  <!-- col -->
+            </div>  <!-- row -->
+        </div>  <!-- container -->
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+        <div class="container-fluid content">
+            <div class="row">
+                <div class="col">
+                    <section class="hero is-large">
+                        <div class="hero-body bg-img">
+                            <p class="title">
+                                Resume | 履歴書を簡単に作成
+                            </p>
+                            <p class="subtitle">
+                                <button class="content_boxlink" type="button" onclick="location.href='/home'">サンプルを見てみる</button>
+                            </p>
+                        </div>
+                    </section>
+                </div>  <!-- col -->
+            </div>  <!-- row -->
         </div>
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </body>
 </html>
